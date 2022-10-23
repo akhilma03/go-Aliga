19c7c644307806ff29c52c9ce0cd5ceee9079de4
Akhil M A <akhilma4@gmail.com>
2022-10-23 15:39:11 +0530

    address

--- goaliga/payment/urls.py
+++ goaliga/payment/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('pay/', start_payment, name="payment"),
     path('payment/success/', handle_payment_success, name="payment_success"),
     path('payz/', views.temp_payment, name="payy"),
-    path('statusz/', views.paymentstatus, name="status")
+    path('statusz/', views.paymentstatus, name="status"),
+    path('address/',views.DetailsPassenger,name ="address")
 ]
